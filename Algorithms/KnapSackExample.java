ce1dae411036e22d194918ecb781995df17ce1a8
Harsh061 <parakhharshit@gmail.com>
2016-02-11 19:41:57 +0530

    Updated code

--- Algorithms/KnapSackExample.java
+++ Algorithms/KnapSackExample.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 class KnapSackExample {
 	public static void main(String ...k) {
 		Scanner sc = new Scanner(System.in);
-		float W = 0F,temp=0F;
+		float W = 0F,temp=0F, temp1=0F, temp2=0F, sum=0F;
 		int i,j;
 		float w[] = new float[5];
 		float v[] = new float[5];
 		float p[] = new float[5];
+		float x[] = new float[5];
 		System.out.println("Enter the weights :");
 		for(i=0;i<5;i++){
 			w[i] = sc.nextFloat();
@@ -27,28 +28,40 @@ class KnapSackExample {
 					temp = p[j];
 					p[j]=p[j+1];
 					p[j+1]=temp;
+					
+					temp1 = w[j];
+					w[j] = w[j+1];
+					w[j+1] = temp1;
+					
+					temp2 = v[j];
+					v[j] = v[j+1];
+					v[j+1] = temp2;
 				} 
-				else
-					p[j]=p[j];
 			}
 		}
 		for(i=0;i<5;i++){
 			System.out.println("The sorted profits are:" +p[i]);
+			System.out.println("The sorted weigths are:" +w[i]);
+			System.out.println("The sorted values are:" +v[i]);
 		}
 		for(i=0;i<5;i++){
 			if(W>w[i]) {
 				W=W-w[i];
-				v[i]=1;
+				x[i]=1;
 				System.out.println("Remaining weight:" +W);
 				System.out.println("Corresponding value:" +v[i]);
 			}
 			else {
 				W=W/w[i];
-				v[i]=W;
-				System.out.println("Remaining weight:" +W);
-				System.out.println("Corresponding value:" +v[i]);
+				x[i]=W;
 				W=0;
+				System.out.println("Remaining weight:" +W);
+				System.out.println("Corresponding value:" +x[i]);
 			}
 		}
+		for(i=0;i<5;i++) {
+			sum = sum + v[i]*x[i];
+		}
+		System.out.println("Optimal value:" +sum);
 	}
 }
